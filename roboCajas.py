9a9bd1fad36e75a9fe71f582fa7a033a5a67cb31
alexlohe25 <A01733984@itesm.mx>
2021-11-22 15:02:42 -0600

    Version 2 - Stacks desnivelados

--- roboCajas.py
+++ roboCajas.py
@@ -14,6 +14,14 @@ class WallBlock(Agent):
         self.pos = pos
     def step(self):
         pass
+class Stack(Agent):
+    def __init__(self, model, pos):
+        super().__init__(model.next_id(), model)
+        self.pos = pos
+        self.model = model
+        self.boxCounter = 0
+    def step(self):
+        pass
 class Caja(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
@@ -24,51 +32,84 @@ class Robot(Agent):
         super().__init__(model.next_id(), model)
         self.pos = pos
         self.model = model
+        self.gotBox = False
         self.matrix = self.model.matrix
         grid = GridPath(matrix = self.matrix)
         self.grid = grid
 
     def step(self):
-        
+        if not self.gotBox:
+            self.collectBox()
+        else:
+            self.leaveBox()
+
+
+    def collectBox(self):
         minPath = 100
         self.grid.cleanup()
         grid = GridPath(matrix= self.matrix)
         self.grid = grid
         start = self.grid.node(self.pos[0], self.pos[1])
         allBoxesinStack = bool(self.model.initialBoxesPos)
+        finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
         if allBoxesinStack:
             cajacerca = self.pos
+            for boxPos in self.model.initialBoxesPos.keys():
+                self.grid.cleanup()
+                grid = GridPath(matrix= self.matrix)
+                self.grid = grid
+                endTemp = self.grid.node(boxPos[0], boxPos[1])
+                pathTemp, runsTemp = finder.find_path(start, endTemp, self.grid)
+                if minPath > len(pathTemp):
+                    cajacerca = boxPos
+                    minPath = len(pathTemp)
         else:
             cajacerca = (1,1)
-        finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
-        print("---------------- CAJAS Y DISTANCIA ------------------")
-        for boxPos in self.model.initialBoxesPos.keys():
-            self.grid.cleanup()
-            grid = GridPath(matrix= self.matrix)
-            self.grid = grid
-            endTemp = self.grid.node(boxPos[0], boxPos[1])
-            pathTemp, runsTemp = finder.find_path(start, endTemp, self.grid)
-            print (boxPos, " -> ",len(pathTemp))
-            if minPath > len(pathTemp):
-                cajacerca = boxPos
-                minPath = len(pathTemp)
-        
-        print ("----------CAJA MAS CERCANA ----------")
-        print(cajacerca)
         self.grid.cleanup()
         grid = GridPath(matrix= self.matrix)
         self.grid = grid
         end = self.grid.node(cajacerca[0], cajacerca[1])
         path, runs = finder.find_path(start, end, self.grid)
-        print ("----------CAMINO MAS CERCANA ----------")
-        print (self.pos," -> ",path)
         if(len(path) > 1):
             next_move = path[1]
             if next_move in self.model.initialBoxesPos.keys():
                 self.model.boxesFound.append(self.model.initialBoxesPos[next_move])
                 self.model.initialBoxesPos.pop(next_move)
-            
+                self.gotBox = True
             self.model.grid.move_agent(self, next_move)
+    
+    
+    def leaveBox(self):
+        minPath = 100
+        self.grid.cleanup()
+        grid = GridPath(matrix= self.matrix)
+        self.grid = grid
+        start = self.grid.node(self.pos[0], self.pos[1])
+        stackNear = self.pos
+        finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
+        for stack in self.model.stacksPos.keys():
+            if (self.model.stacksPos[stack].boxCounter < 5):
+                self.grid.cleanup()
+                grid = GridPath(matrix= self.matrix)
+                self.grid = grid
+                endTemp = self.grid.node(stack[0], stack[1])
+                pathTemp, runsTemp = finder.find_path(start, endTemp, self.grid)
+                if minPath > len(pathTemp):
+                    minPath = len(pathTemp)
+                    stackNear = stack
+        self.grid.cleanup()
+        grid = GridPath(matrix= self.matrix)
+        self.grid = grid
+        end = self.grid.node(stackNear[0], stackNear[1])
+        path, runs = finder.find_path(start, end, self.grid)
+        if(len(path) > 1):
+            next_move = path[1]
+            if next_move in self.model.stacksPos.keys():
+                self.model.stacksPos[next_move].boxCounter += 1
+                self.gotBox = False
+            self.model.grid.move_agent(self, next_move)
+                
+
 
 
 
@@ -79,7 +120,11 @@ class Maze(Model):
         self.grid = Grid(10, 10, torus=False)
         self.amountOfAgents = amountOfAgents
         self.amountOfBoxes = amountOfBoxes
+        self.amountOfStacks = amountOfBoxes//5
+        if amountOfBoxes % 5 > 0:
+            self.amountOfStacks += 1
         self.initialBoxesPos = {}
+        self.stacksPos = {}
         self.boxesFound = []
         self.matrix = [
             [0,0,0,0,0,0,0,0,0,0],
@@ -98,6 +143,16 @@ class Maze(Model):
                 block = WallBlock(self, (x, y))
                 self.grid.place_agent(block, block.pos)
                 self.schedule.add(block)
+        i = 8
+        for j in range(1,self.amountOfStacks + 1):
+            if (j > 8):
+                j = 1
+                i -= 1
+            stack = Stack(self,(i,j))
+            self.grid.place_agent(stack, (j , i))
+            self.schedule.add(stack)
+            self.stacksPos[(j,i)] = stack
+
 
 
         for i in range(self.amountOfBoxes):
@@ -118,21 +173,25 @@ class Maze(Model):
 
     def step(self):
         self.schedule.step()
-        print(self.boxesFound)
         for boxToMove in self.boxesFound:
             self.grid.remove_agent(boxToMove)
+            self.schedule.remove(boxToMove)
             self.boxesFound.remove(boxToMove)
+        for stack in self.stacksPos:
+            print (self.stacksPos[stack].boxCounter)
 
 def agent_portrayal(agent):
     if type(agent) is Robot:
         return {"Shape": "robot.png", "Layer": 0}
     elif type(agent) is Caja:
         return {"Shape": "caja.png", "Layer": 0}
+    elif type(agent) is Stack:
+         return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "#343434", "Layer": 0}
     else:
         return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "Gray", "Layer": 0}
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 450, 450)
 
-server = ModularServer(Maze, [grid], "Act_Integradora_A01733984", {'amountOfAgents':4, 'amountOfBoxes': 10})
+server = ModularServer(Maze, [grid], "Act_Integradora_A01733984", {'amountOfAgents':5, 'amountOfBoxes': 11})
 server.port = 8522
 server.launch()
