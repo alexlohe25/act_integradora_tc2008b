cf3990d8124fc1a7e12df94a8d6cc3d2065f34ec
alexlohe25 <A01733984@itesm.mx>
2021-11-26 11:37:22 -0600

    Codigo comentado API y Modelo de Mesa

--- backend.py
+++ backend.py
@@ -23,17 +23,15 @@ def queryState(id):
     model = games[id]
     model.step()
     listaRobots = []
+    #buscar todos los agentes del modelo y si son cajas o robots incluirlos en el arreglo de diccionarios a mandar al json
     for i in range(len(model.schedule.agents)):
         ghost = model.schedule.agents[i]
         if type(ghost) is roboCajas.Robot:
             listaRobots.append({"x": ghost.pos[0], "y": ghost.pos[1], "tipo" : "Robot", "inStack" : 0, "inRobot" : False})
         elif type(ghost) is roboCajas.Caja:
             listaRobots.append({"x": ghost.pos[0], "y": ghost.pos[1], "tipo" : "Caja", "inStack" : ghost.inStack , "inRobot" : ghost.inRobot})
-        #elif type(ghost) is roboCajas.Stack:
-        #    listaRobots.append({"x": ghost.pos[0], "y": ghost.pos[1], "tipo" : "Stack"})
         else:
             i = i - 1
-    #print (listaRobots)
     return jsonify({"Items":listaRobots})
 
 app.run()
--- roboCajas.py
+++ roboCajas.py
@@ -9,13 +9,14 @@ from pathfinding.core.grid import Grid as GridPath
 from pathfinding.finder.a_star import AStarFinder
 
 import sys
-
+#agente para delimitar los muros
 class WallBlock(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
     def step(self):
         pass
+#agente que funge como pila de cajas, su principal atributo es el contador
 class Stack(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
@@ -24,12 +25,14 @@ class Stack(Agent):
         self.boxCounter = 0
     def step(self):
         pass
+#agente Caja, que tiene 2 flags para indicar si se encuentra en una pila de altura n o si la caja está recogida por un Robot
 class Caja(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
         self.inStack = 1
         self.inRobot = False
+#agente Robot, que posee 2 movimientos principales: ir a buscar una caja y dejar dicha caja en una pila
 class Robot(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
@@ -41,13 +44,16 @@ class Robot(Agent):
         self.grid = grid
 
     def step(self):
+        #si no tiene una caja por llevar, la busca
         if not self.gotBox:
             self.collectBox()
         else:
+            #si la tiene, la dejará en la pila más cercana que tenga menos de 5 cajas apiladas
             self.leaveBox()
 
-
+    #metodo que aplica el algoritmo AStar a todas las cajas en posiciones iniciales
     def collectBox(self):
+        #inicializadores del AStar
         minPath = 100
         self.grid.cleanup()
         grid = GridPath(matrix= self.matrix)
@@ -55,6 +61,7 @@ class Robot(Agent):
         start = self.grid.node(self.pos[0], self.pos[1])
         allBoxesinStack = bool(self.model.initialBoxesPos)
         finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
+        #si todavia hay cajas por acomodar, busca la caja mas cercana por el AStar comparando tamaño del arreglo de pasos
         if allBoxesinStack:
             cajacerca = self.pos
             for boxPos in self.model.initialBoxesPos.keys():
@@ -67,25 +74,28 @@ class Robot(Agent):
                     cajacerca = boxPos
                     minPath = len(pathTemp)
         else:
+            #si estan acmododadas, el robot se mueve a la casilla 1,1
             cajacerca = (1,1)
+        #aplicar una vez mas AStar con el camino mas cercano encontrado
         self.grid.cleanup()
         grid = GridPath(matrix= self.matrix)
         self.grid = grid
         end = self.grid.node(cajacerca[0], cajacerca[1])
         path, runs = finder.find_path(start, end, self.grid)
         if(len(path) > 1):
+            #si el siguiente movimiento es la posicion de la caja, activas las flags de dicha caja y procedes de dejar la caja
+            # a la pila mas cercana con el booleano gotBox
             next_move = path[1]
             if cajacerca == self.pos:
                 next_move = path[0]
             if next_move in self.model.initialBoxesPos.keys():
                 self.carryingBox = self.model.initialBoxesPos[next_move]
                 self.carryingBox.inRobot = True
-                #self.model.boxesFound.append(self.model.initialBoxesPos[next_move])
                 self.model.initialBoxesPos.pop(next_move)
                 self.gotBox = True
             self.model.grid.move_agent(self, next_move)
     
-    
+    #metodo para dejar la caja del robot en la pila mas cercana que tenga menos de 5 cajas empleando aStar
     def leaveBox(self):
         minPath = 100
         self.grid.cleanup()
@@ -94,6 +104,7 @@ class Robot(Agent):
         start = self.grid.node(self.pos[0], self.pos[1])
         stackNear = self.pos
         finder = AStarFinder(diagonal_movement = DiagonalMovement.never)
+        #busqueda de la pila más cercana con menos de 5 cajas almacenadas por AStar
         for stack in self.model.stacksPos.keys():
             if (self.model.stacksPos[stack].boxCounter < 5):
                 self.grid.cleanup()
@@ -104,6 +115,7 @@ class Robot(Agent):
                 if minPath > len(pathTemp):
                     minPath = len(pathTemp)
                     stackNear = stack
+        #implementacion de AStar con el camino más cercano
         self.grid.cleanup()
         grid = GridPath(matrix= self.matrix)
         self.grid = grid
@@ -111,20 +123,17 @@ class Robot(Agent):
         path, runs = finder.find_path(start, end, self.grid)
         if(len(path) > 1):
             next_move = path[1]
+            #si el siguiente movimiento del path es un stack y ese stack tiene menos de 5 cajas
             if next_move in self.model.stacksPos.keys() and self.model.stacksPos[next_move].boxCounter < 5:
-                #caja = Caja(self.model, next_move)
-                
-                #self.model.grid.place_agent(caja, next_move)
-                #self.model.schedule.add(caja)
+                #asignamos las flags de la caja con los valores del orden en el que entro al stack
                 self.model.stacksPos[next_move].boxCounter += 1
                 self.carryingBox.inStack = self.model.stacksPos[next_move].boxCounter
                 self.carryingBox.inRobot = False
+                #el robot vuelve a buscar la caja desordenada más cercana
                 self.gotBox = False
-                #if self.model.stacksPos[next_move].boxCounter == 5:
-                #    self.model.matrix[self.pos[0]][self.pos[1]] = 0
             self.carryingBox.pos = next_move
             self.model.grid.move_agent(self, next_move)
-                
+#Modelo del almacen, en donde se inicializa el grid , la cantidad de agentes a interactuar                
 class Maze(Model):
     def __init__(self):
         super().__init__()
@@ -135,7 +144,6 @@ class Maze(Model):
         self.amountOfBoxes = 20
         self.initialBoxesPos = {}
         self.stacksPos = {}
-        self.boxesFound = []
         self.allBoxesOrdered = False
         self.steps = 0
         self.matrix = [
@@ -150,6 +158,7 @@ class Maze(Model):
             [0,1,1,1,1,1,1,1,1,0],
             [0,0,0,0,0,0,0,0,0,0]
         ]
+        #establecimiento de muros
         for _,x,y in self.grid.coord_iter():
             if self.matrix[y][x] == 0:
                 block = WallBlock(self, (x, y))
@@ -157,9 +166,11 @@ class Maze(Model):
                 self.schedule.add(block)
             else:
                 self.availableCells += 1
+        #validar que las cajas entren en el grid
         self.availableCells -= self.amountOfAgents
         if (self.amountOfBoxes > self.availableCells):
             self.amountOfBoxes = 30
+        #obtener la cantidad de pilas a insertar en el grid
         self.amountOfStacks = self.amountOfBoxes//5
         if self.amountOfBoxes % 5 > 0:
             self.amountOfStacks += 1
@@ -172,7 +183,7 @@ class Maze(Model):
             self.grid.place_agent(stack, (j , k))
             self.schedule.add(stack)
             self.stacksPos[(j,k)] = stack
-
+        #insertar robots en el grid
         for i in range(self.amountOfAgents):
             random_pos = self.grid.find_empty()
             x = random_pos[1]
@@ -183,7 +194,7 @@ class Maze(Model):
                 self.schedule.add(bender)
             else:
                 i = i - 1
-
+        #insertar cajas en el grid
         for i in range(self.amountOfBoxes):
             random_pos = self.grid.find_empty()
             x = random_pos[1]
@@ -199,14 +210,12 @@ class Maze(Model):
         boxesInStack = 0
         self.schedule.step()
         self.steps += 1
-        for boxToMove in self.boxesFound:
-            self.grid.remove_agent(boxToMove)
-            self.schedule.remove(boxToMove)
-            self.boxesFound.remove(boxToMove)
+        #imprime los contadores de cada stack
         for stack in self.stacksPos:
             if not self.allBoxesOrdered:
                 print ("Cajas en Stack ",stack," -> ",self.stacksPos[stack].boxCounter)
                 boxesInStack += self.stacksPos[stack].boxCounter
+        #si todas las cajas ya se ordenaron se despliega la información de la simulación
         if boxesInStack == self.amountOfBoxes and not self.allBoxesOrdered:
             self.allBoxesOrdered = True
             print ("-------- TODAS LAS CAJAS ORDENADAS EN PILAS ----------")
